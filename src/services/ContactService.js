08e6a77e2c021bbffeaec13196b5a26e462e14d0
candice <candiceschramm@gmail.com>
2019-06-01 19:12:24 -0600

    form adds new contact, with name required and favorite status

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -1,5 +1,15 @@
 import api from '@/api'
 
 export default {
-  getContacts: () => api().get(`/contacts`)
+  getContacts: () => api().get(`/contacts`),
+  // another method to PUT new contact, pass in the req.body contact info
+  addContact: (contact) => {
+    api().post('/contact', contact)
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
 }
